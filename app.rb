69f4fccccf22dd91c53e6ef8b52654dade2cb245
Jose Melero Martinez <jmelero08@gmail.com>
2021-01-08 01:25:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   get '/favorite-song' do
-    " My favorite-song is This Is The House That Doubt Built"
+    " My favorite song is This Is The House That Doubt Built"
   end
   
 end
