3146df1332145439f10167810e7bf1a418c9c3c3
Jose Melero Martinez <jmelero08@gmail.com>
2021-01-08 01:24:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ class App < Sinatra::Base
   get '/name' do 
     "My name is Jose"
   end
+  
+  get '/hometown' do 
+    "My hometown is Phoenix"
+  end
+
+  get '/favorite-song' do
+    "This Is The House That Doubt Built"
+  end
+  
 end
